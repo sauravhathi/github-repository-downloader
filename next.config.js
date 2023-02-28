95b3bd96a6b0f9f7419a3b475efc8a7184c12431
Saurav Hathi <61316762+sauravhathi@users.noreply.github.com>
2023-03-01 03:09:46 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,5 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  async rewrites() {
-    return [
-      {
-        source: '/:path*',
-        destination: 'https://api.github.com/repos/:path*',
-      },
-    ]
-  },
 }
 
