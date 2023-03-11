971d5969dbb35e273c054819755e98078760d255
Saurav Hathi <61316762+sauravhathi@users.noreply.github.com>
2023-03-11 15:54:08 +0530

    removed proxy

--- next.config.js
+++ next.config.js
@@ -1,12 +1,4 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  async rewrites() {
-    return [
-      {
-        source: '/:path*',
-        destination: 'https://api.github.com/repos/:path*',
-      },
-    ]
-  },
 }
